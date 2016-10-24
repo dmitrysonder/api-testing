a8699b5426558b736201efcbe99cae6315c7f704
dmitrysonder <dmitry.sonder@gmail.com>
2016-10-24 12:02:50 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import jsonschema
+
+schema = open("schemes/schema.json").read()
+
+url = 'http://appybuy.korbit.eu:8080/magnoliaPublic/.rest/appybuy/v1/charities'
+payload = {'count':222, 'start':0, 'sort':'richest', 'lang':'eng'}
+
+r = requests.get(url, params=payload)
+
+try:
+    v = jsonschema.Draft4Validator(json.loads(schema))
+    for error in sorted(v.iter_errors(json.loads(r.text)), key=str):
+        print(error.message)
+except jsonschema.ValidationError as e:
+    print(e.message)
+
+
